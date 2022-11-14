77c1582ea283e937f1a49375dccba1097223072b
Kaustubh <kaustubhjoshir@gmail.com>
2022-11-14 21:13:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+require('dotenv').config();
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.get('/', (req, res)=>{
+    res.status(200).send('hello world');
+})
+app.use((req, res)=>{
+    res.status(404).send('Not found');
+});
+
+app.listen(process.env.PORT || 3000);
