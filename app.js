6347d747e0e5cd8812bad617324b4f68c191a1a5
Kaustubh <kaustubhjoshir@gmail.com>
2022-11-16 16:00:12 +0530

    second edition

--- app.js
+++ app.js
@@ -6,36 +6,43 @@ const responseTime = require('response-time')
 var app = express();
 
 app.set('trust proxy', true);
-app.use(responseTime((req, res, time) => {
-    let url = req.protocol + '://' + req.get('host') + req.originalUrl;
-    let userip = req.headers['x-forwarded-for'];
-    let date = new Date();
-    let timestamp = date.getFullYear()+"/"+(date.getMonth()+1) +"/"+ date.getDate() +"/"+ date.getHours() +":"+ date.getMinutes() +":"+ date.getSeconds() +":"+ date.getMilliseconds();
-    let latency = Number(time.toFixed(3));
-    log_file.write(`{${url}, ${userip}, ${timestamp}, ${latency}}\n`);
-  }));
+app.use((req, res, next) => {
+    req.time = Date.now();
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 var fs = require('fs');
 var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
 
+function logger(req) {
+    let url = req.protocol + '://' + req.get('host') + req.originalUrl;
+    let userip = req.headers['x-forwarded-for'];
+    let timestamp = new Date();
+    let latency = Number((Date.now() - req.time).toFixed(3));
+    log_file.write(`{URL: ${url}, IP: ${userip}, DATE: ${timestamp}, LATENCY: ${latency}ms}\n`);
+}
 app.get('/url1', (req, res)=>{
     res.status(200).send('hello ' +  req.query.query + ' world');
+    logger(req);
 })
 app.get('/url2', (req, res)=>{
     res.status(200).send('hello ' +  req.query.query + ' world');
+    logger(req);
 })
 app.get('/url3', (req, res)=>{
-
     res.status(200).send('hello ' +  req.query.query + ' world');
+    logger(req);
 })
 app.get('/logs', (req, res)=>{
     let readStream = fs.createReadStream(__dirname + '/debug.log');
     readStream.on('open', ()=>readStream.pipe(res));
+    logger(req);
 })
 app.use((req, res)=>{
     res.status(404).send('Not found');
+    logger(req);
 });
 
 app.listen(3000);
